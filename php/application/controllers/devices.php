94b17e922cb3ccfb00a28d85621e24639b8004cd
Martin Rabaglia <tinchoforever@gmail.com>
2013-04-16 12:05:22 -0300

    Laravel Basic setup

---
+++ php/application/controllers/devices.php
@@ -0,0 +1,20 @@
+<?php
+
+class Devices_Controller extends Base_Controller {
+
+	public function action_index()
+    {
+        return "hola!";
+    }
+
+	public function action_show()
+    {
+
+    }
+
+	public function action_edit()
+    {
+
+    }
+
+}
